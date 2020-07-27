6a82b04a24b58624e3ee2fbd1f603f27616da9e7
nitsuga5124 <nitsuga5124@gmail.com>
2020-07-28 00:18:23 +0200

    Typo 	modified:   src/gateway.rs

--- src/gateway.rs
+++ src/gateway.rs
@@ -8,7 +8,7 @@ use tokio::sync::Mutex;
 
 #[async_trait]
 pub trait LavalinkEventHandler {
-    /// Periodic event that returns the stadistics of the server.
+    /// Periodic event that returns the statistics of the server.
     async fn stats(&self, _client: Arc<Mutex<LavalinkClient>>, _event: Stats) {}
     /// Event that triggers when a player updates.
     async fn player_update(&self, _client: Arc<Mutex<LavalinkClient>>, _event: PlayerUpdate) {}
@@ -21,6 +21,8 @@ pub trait LavalinkEventHandler {
 /*
 
 {"playingPlayers":1,"op":"stats","memory":{"reservable":4294967296,"used":513694368,"free":262251872,"allocated":775946240},"frameStats":{"sent":3000,"deficit":0,"nulled":0},"players":2,"cpu":{"cores":8,"systemLoad":0.12922594961493278,"lavalinkLoad":0.0020833333333333333},"uptime":732761629}
+
+{"playingPlayers":0,"op":"stats","memory":{"reservable":4294967296,"used":496493304,"free":344464648,"allocated":840957952},"players":1,"cpu":{"cores":8,"systemLoad":0.25,"lavalinkLoad":0.40552793689939176},"uptime":797552035}
 
 
 {"op":"playerUpdate","state":{"position":354760,"time":1595819222861},"guildId":"182892283111276544"}
