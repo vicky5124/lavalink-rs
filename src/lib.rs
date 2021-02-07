0d05cb614b63f082d762e10ea09ceaaf13102087
nitsuga5124 <nitsuga5124@gmail.com>
2021-02-07 23:36:46 +0100

    Added missing features warning 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,9 @@ use async_tungstenite::{
     },
 };
 
+#[cfg(all(not(feature = "rustls-marker"), not(feature = "native-marker")))]
+compile_error!("Please specify a feature, either `rustls` or `native`");
+
 pub const EQ_BASE: [f64; 15] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
 pub const EQ_BOOST: [f64; 15] = [-0.075, 0.125, 0.125, 0.1, 0.1, 0.05, 0.075, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.15, 0.05];
 pub const EQ_METAL: [f64; 15] = [0.0, 0.1, 0.1, 0.15, 0.13, 0.1, 0.0, 0.125, 0.175, 0.175, 0.125, 0.125, 0.1, 0.075, 0.0];
