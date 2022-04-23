a6167b4014bbcc547a2ffb54184a302eb373dc26
vicky5124 <vickyf5124@gmail.com>
2022-04-23 22:24:18 +0200

    Use the next branch on songbird and serenity 	modified:   Cargo.toml 	modified:   build.rs

--- build.rs
+++ build.rs
@@ -1,14 +1,17 @@
 #[cfg(all(not(feature = "rustls"), not(feature = "native")))]
 compile_error!("Please specify a feature, either `rustls` or `native`.");
 
+#[cfg(all(feature = "rustls", feature = "native"))]
+compile_error!("Please specify a single TLS features, either `rustls` or `native`, but not both (hint: `default-features = false`).");
+
 #[cfg(all(not(feature = "songbird"), not(feature = "discord-gateway")))]
 compile_error!("Set either `songbird` or `discord-gateway` as a feature to be able to connect to voicce channels.");
 
 #[cfg(all(not(feature = "tracing-log"), not(feature = "normal-log")))]
-compile_error!("Set either `tracing-log` or `normal-log` as your logging crate.");
+compile_error!("Set either `tracing-log` or `normal-log` in your features.");
 
 #[cfg(all(feature = "tracing-log", feature = "normal-log"))]
-compile_error!("You can only choose between `tracing-log` and `normal-log` as your logging crate.");
+compile_error!("You can only choose one of `tracing-log` and `normal-log` as your logging crate. (hint: `default-features = false`)");
 
 use version_check::Version;
 
